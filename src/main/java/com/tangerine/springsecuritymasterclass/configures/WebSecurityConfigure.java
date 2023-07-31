113e77ce771bfe697efb88eac8123d0ec94a35ce
SY97P <onetuks@naver.com>
2023-08-01 01:37:17 +0900

    feat: mission1 클리어

--- src/main/java/com/tangerine/springsecuritymasterclass/configures/WebSecurityConfigure.java
+++ src/main/java/com/tangerine/springsecuritymasterclass/configures/WebSecurityConfigure.java
@@ -5,12 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigure {
 
+    // HttpSecurity : 세부적인 웹 보안기능 설정 처리 api 제공
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -21,8 +28,49 @@ public class WebSecurityConfigure {
                         .permitAll())
                 .formLogin(login -> login
                         .defaultSuccessUrl("/login")
-                        .permitAll());
+                        .permitAll())
+                .rememberMe(remember -> remember
+                        .rememberMeCookieName("remember-me")
+                        .rememberMeParameter("remember-me")
+                        .tokenValiditySeconds(300)
+                        .alwaysRemember(false))
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("remember-me"));
         return http.build();
+    }
+
+    // 정적리소스는 spring security filter를 걸어주지 않음
+    // *.html, *.css, *.js
+    // 불필요한 서버 자원 낭비 방지
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers("/assets/**");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User
+                .withUsername("user")
+                .password(passwordEncoder.encode("user123"))
+                .roles("USER")
+                .build());
+        manager.createUser(User
+                .withUsername("admin")
+                .password(passwordEncoder.encode("admin123"))
+                .roles("ADMIN")
+                .build());
+        return manager;
     }
 
 //    @Bean
@@ -36,11 +84,5 @@ public class WebSecurityConfigure {
 //                        .permitAll())
 //                .build();
 //    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring()
-                .requestMatchers("/assets/**");
-    }
 
 }
--- src/test/java/com/tangerine/springsecuritymasterclass/ApplicationTests.java
+++ src/test/java/com/tangerine/springsecuritymasterclass/ApplicationTests.java
@@ -1,13 +1,21 @@
 package com.tangerine.springsecuritymasterclass;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class ApplicationTests {
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Test
     void contextLoads() {
+        String encodedPassword = passwordEncoder.encode("user1234");
+
+        System.out.println(encodedPassword);
     }
 
 }
